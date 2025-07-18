1269443d93fb9c13c2312d425216b2ed4c08b773
jungminna03 <jungminna03@gmail.com>
2025-07-18 14:13:58 +0900

    Refactoring UnitStat

--- Assets/Scripts/Combat/UnitStat.cs
+++ Assets/Scripts/Combat/UnitStat.cs
@@ -1,63 +1,47 @@
 using UnityEngine;
 using DataEntity;
-using DataEnum;
 using System;
-using UnityEditor.Build;
 
 public class UnitStat
 {
-    private EntityData myData;
+    private EntityData _baseData;
 
-    public Action m_HPChanged;
-    public Action m_APChanged;
-    public Action m_Die;
+    public Action<float, float> OnHPChanged;
+    public Action<int, int> OnAPChanged;
+    public Action OnDie;
 
-    private float max_HP;       public float Max_HP { get { return max_HP; } }
-    private float cur_HP;       public float Cur_HP { get { return cur_HP; } }
-    private int max_AP = 9;     public int Max_AP { get { return max_AP; } }
-    private int cur_AP;         public int Cur_AP {  get { return cur_AP; } }
+    public float Max_HP { get => (float)_baseData.Default_HP; }
+    
+    private float _curHp;       
+    public float Cur_HP { get { return _curHp; } }
+    
+    public int Max_AP { get => _baseData.Default_AP; }
+    
+    private int _curAp;         
+    public int Cur_AP {  get { return _curAp; } }
 
-    public void IntializeStat(EntityData data)
+    public UnitStat(EntityData baseData)
     {
-        myData = data;
-
-        max_HP = (float)myData.Default_HP;
-        cur_HP = max_HP;
-        cur_AP = myData.Default_AP;
-    }
-
-    public void OnIntializeHUD()
-    {
-        m_HPChanged?.Invoke();
-        m_APChanged?.Invoke();
+        _baseData = baseData;
+        
+        _curHp = (float)baseData.Default_HP;
+        _curAp = baseData.Default_AP;
     }
 
     public void GetDamaged(float value)
     {
-        cur_HP = Mathf.Clamp(cur_HP - value, 0f, max_HP);
-        m_HPChanged();
+        _curHp = Mathf.Clamp(_curHp - value, 0f, Max_HP);
+        OnHPChanged.Invoke(_curHp, Max_HP);
 
         if (Cur_HP <= 0f)
-            m_Die();
+            OnDie.Invoke();
     }
 
     public void GetHealed(float value)
     {
-        cur_HP = Mathf.Clamp(cur_HP + value, 0f, max_HP);
-        m_HPChanged();
+        _curHp = Mathf.Clamp(_curHp + value, 0f, Max_HP);
+        OnHPChanged.Invoke(_curHp, Max_HP);
     }
 
-    public void OnSkillAttack()
-    {
-        cur_AP = Mathf.Clamp(cur_AP - 1, 0, max_AP);
-        m_APChanged();
-    }
-
-    public void OnNormalAttack()
-    {
-        cur_AP = Mathf.Clamp(cur_AP + 1, 0, max_AP);
-        m_APChanged();
-    }
-
-    public EntityData GetData() { return myData; }
+    public EntityData GetData() { return _baseData; }
 }
