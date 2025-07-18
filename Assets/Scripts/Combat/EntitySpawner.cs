fdf9951e208890df77661282218ed7175c555c1e
jungminna03 <jungminna03@gmail.com>
2025-07-18 14:14:32 +0900

    Refactoring EntitiySpawner

--- Assets/Scripts/Combat/EntitySpawner.cs
+++ Assets/Scripts/Combat/EntitySpawner.cs
@@ -7,25 +7,18 @@ using Unity.VisualScripting;
 
 public class EntitySpawner : MonoBehaviour
 {
-    public bool isPlayerSide;
-    public List<GameObject> spawnPos;
-
-    public BaseUnit CreateUnit(EntityData entityData, HUDManager hudManager, int index)
+    public PlayerUnit CreatePlayerUnit(EntityData entityData)
     {
-        GameObject prefab;
-        prefab = isPlayerSide == true ? AssetLoader.LoadCharacterPrefabAsset(entityData.Asset_File) : AssetLoader.LoadMonsterPrefabAsset(entityData.Asset_File);
-        BaseUnit unit = Instantiate(prefab, Vector2.zero, Quaternion.identity).GetComponent<BaseUnit>();
-        unit.transform.SetParent(spawnPos[index].transform, false);
-
-        if (unit != null)
-        {
-            unit.Initialize(entityData, hudManager);
-        }
-        else
-        {
-            Debug.LogWarning("유닛에 스크립트 할당이 되어있는지 확인하십시오!!!");
-        }
-
-        return unit;
+        GameObject go = AssetLoader.LoadCharacterPrefabAsset(entityData.Asset_File);
+        BaseUnit unit = Instantiate(go, Vector2.zero, Quaternion.identity).GetComponent<BaseUnit>();
+        
+        return unit as PlayerUnit;
+    }
+    public EnemyUnit CreateEnemyUnit(EntityData entityData)
+    {
+        GameObject go = AssetLoader.LoadCharacterPrefabAsset(entityData.Asset_File);
+        BaseUnit unit = Instantiate(go, Vector2.zero, Quaternion.identity).GetComponent<BaseUnit>();
+        
+        return unit as EnemyUnit;
     }
 }
