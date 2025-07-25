612ad704d336f76acad265064b6055ee80d20629
jungminna03 <jungminna03@gmail.com>
2025-07-25 16:56:53 +0900

    Update InputHandler
    TODO : Update using unity input system

--- Assets/Scripts/Core/InputHandler.cs
+++ Assets/Scripts/Core/InputHandler.cs
@@ -2,7 +2,25 @@ using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "InputHandler", menuName = "Core/InputHandler", order = 1)]
-class InputHandler : ScriptableObject
+class InputHandler : ScriptableObject, IUpdateObserver
 {
+    private void Awake()
+    {
+        UpdatePublisher.SubscribeObserver(this);
+    }
+
     public Action<KeyCode> OnInputButtonDown;
+    public void ObserverUpdate(float dt)
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    OnInputButtonDown?.Invoke(key);
+                }
+            }
+        }
+    }
 }
