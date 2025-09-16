a3c22d4dfbfb8ef121cfe52d5a94bc313cff484f
YoungJunGit <eimuc00@gmail.com>
2025-09-16 20:03:13 +0900

    <FIX> EventRegistry

--- Assets/Scripts/Core/EventRegistry.cs
+++ Assets/Scripts/Core/EventRegistry.cs
@@ -2,6 +2,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+
+
+public class EventRegistry<T>
+{
+    private Func<T> eventSender;
+    private HashSet<Func<T>> events = new HashSet<Func<T>>();
+
+    public T Call()
+    {
+        return eventSender.Invoke();
+    }
+
+    public void Register(Func<T> callback)
+    {
+        if (events.Add(callback))
+            eventSender += callback;
+        else
+            Debug.LogWarning("Event has been already registered!");
+    }
+
+    public void UnregisterAll()
+    {
+        foreach (Func<T> _event in events)
+        {
+            eventSender -= _event;
+        }
+    }
+}
+
 public class EventRegistry<S, A>
 {
     private Func<S, A> eventSender;
@@ -9,7 +38,7 @@ public class EventRegistry<S, A>
 
     public A Call(S param)
     {
-        return eventSender(param);
+        return eventSender.Invoke(param);
     }
 
     public void Register(Func<S, A> callback)
