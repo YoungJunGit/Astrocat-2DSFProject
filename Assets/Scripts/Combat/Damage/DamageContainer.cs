7445ca7db2321a6006f9e278c654e3d9c125a7c3
YoungJunGit <eimuc00@gmail.com>
2025-09-24 00:12:26 +0900

    <FIX> Damage Container

--- Assets/Scripts/Combat/Damage/DamageContainer.cs
+++ Assets/Scripts/Combat/Damage/DamageContainer.cs
@@ -1,13 +1,27 @@
+using NaughtyAttributes;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "DamageContainer", menuName = "GameScene/Damage/DamageContainer")]
 public class DamageContainer : ScriptableObject
 {
-    [ShowInInspector] public GameObject Value_Prefab { get; set; }
-    [ShowInInspector] public GameObject Digit_Prefab { get; set; }
-    [ShowInInspector] public Sprite[] Damage_Sprites { get; set; }
-    [ShowInInspector] public AnimationCurve JumpEase { get; set; }
-    [ShowInInspector] public AnimationCurve FadeEase { get; set; }
-    [ShowInInspector] public float JumpValue         { get; set; }
+    [SerializeField] 
+    private GameObject     value_Prefab;
+    [SerializeField] 
+    private GameObject     digit_Prefab;
+    [SerializeField] 
+    private Sprite[]       damage_Sprites;
+    [SerializeField, CurveRange(0, 0, 1, 1)] 
+    private AnimationCurve jumpEase;
+    [SerializeField, CurveRange(0, 0, 1, 1)] 
+    private AnimationCurve fadeEase;
+    [SerializeField] 
+    private float          jumpValue;
+
+    public GameObject       Value_Prefab    => value_Prefab;
+    public GameObject       Digit_Prefab    => digit_Prefab;
+    public Sprite[]         Damage_Sprites  => damage_Sprites;
+    public AnimationCurve   JumpEase        => jumpEase;
+    public AnimationCurve   FadeEase        => fadeEase;
+    public float            JumpValue       => jumpValue;
 }
