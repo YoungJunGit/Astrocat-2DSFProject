205133223c4bea9048c0b100ee2764c903f179bd
YoungJunGit <eimuc00@gmail.com>
2025-11-25 21:00:58 +0900

    <FIX> Refactoring DamageEffect

--- Assets/Scripts/Combat/CrowdControl/DamageEffect.cs
+++ Assets/Scripts/Combat/CrowdControl/DamageEffect.cs
@@ -2,17 +2,19 @@ using DataEnum;
 using static CombatEffectManager;
 using static TimelinePublisher;
 
-public class BurnEffect : BaseEffect<float>, IStartTurnEffectProvider
+public abstract class DamageEffect : BaseEffect<float>, IStartTurnEffectProvider
 {
-    public string StartTurnKey => "FIRE_DOT";
+    public abstract string StartTurnKey { get; }
+    public abstract BUFF_TYPE damageType { get; }
+    public abstract UPDATE_TYPE updateType { get; }
 
     protected override BasicStatModifier<float> CreateModifier()
     {
         float value = Info.Value;
 
         var modifer = new BasicStatModifier<float>(
-            BUFF_TYPE.DAMAGE_EACH_TURN, 
-            UPDATE_TYPE.ROUND, 
+            damageType,
+            updateType,
             (v) => v + value
         );
 
@@ -25,4 +27,13 @@ public class BurnEffect : BaseEffect<float>, IStartTurnEffectProvider
         IDamage damage = DamageFactory.CreateNormalDamage<BurnDamageCalculator>(Context.Caster, Context.Target);
         Context.Target.GetStat().GetDamaged(damage);
     }
+}
+
+public class BurnEffect : DamageEffect
+{
+    public override string StartTurnKey => "FIRE_DOT";
+
+    public override BUFF_TYPE damageType => BUFF_TYPE.DAMAGE_EACH_TURN;
+
+    public override UPDATE_TYPE updateType => UPDATE_TYPE.ROUND;
 }
