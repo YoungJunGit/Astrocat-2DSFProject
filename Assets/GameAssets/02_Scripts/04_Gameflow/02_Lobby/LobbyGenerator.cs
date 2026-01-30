065590ff09e8f3d57c3562908b0ef6782bd50d26
yuno0271 <yuno0271@naver.com>
2026-01-30 23:02:46 +0900

    <Dialogue>비동기 처리

---
+++ Assets/GameAssets/02_Scripts/04_Gameflow/02_Lobby/LobbyGenerator.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LobbyGenerator", menuName = "LobbyGenerator")]
+public class LobbyGenerator : ScriptableObject
+{
+    [Header("Background")]
+    [SerializeField]
+    private GameObject cameraRigPref;
+    [SerializeField]
+    private GameObject spaceshipRootPref;
+    [SerializeField]
+    private ParticleSystem starParticle;
+
+    private CameraMove camMove;
+
+    public void Init()
+    {
+        camMove = Instantiate(cameraRigPref).GetComponent<CameraMove>();
+        Instantiate(spaceshipRootPref);
+        Instantiate(starParticle);
+
+        if(camMove != null)
+        {
+            camMove.StartCameraMove();
+            UpdatePublisher.SubscribeObserver(camMove);
+            ServiceLocator.ForSceneOf(this)
+                .Register(camMove as ICameraMove); 
+        }
+    }
+
+    public void DisableCamera()
+    {
+        UpdatePublisher.DiscribeObserver(camMove);
+    }
+}
